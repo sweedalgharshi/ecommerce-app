f5ec9ca17c9cc1c76c783bf07a758bcf20124e98
sweedalgharshi <sweedalgracius@gmail.com>
2025-02-17 16:11:17 +0530

    added hero right side and all fonts necessary

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import { assets } from "../assets/assets";
+
 function Hero() {
   return (
     <div className="flex flex-col sm:flex-row border border-gray-400">
@@ -8,13 +10,17 @@ function Hero() {
             <p className="w-8 md:w-11 h-[2px] bg-[#414141]"></p>
             <p className="font-medium text-sm md:text-base">OUR BESTSELLERS</p>
           </div>
-          <h1 className="text-3xl lg:text-5xl sm:py-3 leading-relaxed">Latest Arrivals</h1>
+          <h1 className="prata-regular text-3xl lg:text-5xl sm:py-3 leading-relaxed">
+            Latest Arrivals
+          </h1>
           <div className="flex items-center gap-2">
             <p className="font-semibold text-sm md:text-base">SHOP NOW</p>
             <p className="w-8 md:w-11 h-[1px] bg-[#414141]"></p>
           </div>
         </div>
       </div>
+      {/* Hero Right Side */}
+      <img src={assets.hero_img} alt="" className="w-full sm:w-1/2" />
     </div>
   );
 }
