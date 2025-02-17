90f01c971802038efd2f48bdd336f126a35b7843
sweedalgharshi <sweedalgracius@gmail.com>
2025-02-17 15:10:32 +0530

    added dropdown-menu for userProfile and cart styling and coded as necessary

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
 
 function NavBar() {
@@ -24,6 +24,28 @@ function NavBar() {
           <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
         </NavLink>
       </ul>
+
+      <div className="flex items-center gap-6">
+        <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+
+        <div className="group relative">
+          <img src={assets.profile_icon} className="w-5 cursor-pointer" alt="" />
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col w-36 gap-2 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
+            </div>
+          </div>
+        </div>
+
+        <Link className="relative" to="/cart">
+          <img src={assets.cart_icon} alt="" className="w-5 min-w-5" />
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+            10
+          </p>
+        </Link>
+      </div>
     </div>
   );
 }
