ed50da372cf3f5e02b87bfb79d0bed670029eeba
sweedalgharshi <sweedalgracius@gmail.com>
2025-02-19 15:01:58 +0530

    added product item component logic and styling to latest collection component

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 
 import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 function LatestCollection() {
   const { products } = useContext(ShopContext);
@@ -21,6 +22,21 @@ function LatestCollection() {
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus, est consectetur atque
           odio inventore sapiente!
         </p>
+      </div>
+
+      {/* Rendering Products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {latestProducts.map((item, index) => {
+          return (
+            <ProductItem
+              key={index}
+              id={item._id}
+              name={item.name}
+              price={item.price}
+              image={item.image}
+            />
+          );
+        })}
       </div>
     </div>
   );
