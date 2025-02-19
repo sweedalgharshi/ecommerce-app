b328f4bf11f78ae5738fff3615b45570d031895c
sweedalgharshi <sweedalgracius@gmail.com>
2025-02-19 15:36:11 +0530

    added best seller component

---
+++ frontend/src/components/BestSeller.jsx
@@ -0,0 +1,41 @@
+import { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
+
+function BestSeller() {
+  const { products } = useContext(ShopContext);
+  const [bestSeller, setBestSeller] = useState([]);
+
+  useEffect(function () {
+    const bestProduct = products.filter((item) => item.bestseller);
+
+    setBestSeller(bestProduct.slice(0, 5));
+  }, []);
+
+  return (
+    <div className="my-10">
+      <div className="text-center py-8 text-3xl">
+        <Title text1="BEST" text2="SELLER" />
+        <p className="w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600 ">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis eligendi delectus,
+          ipsum aspernatur dolorum eius.
+        </p>
+      </div>
+
+      {/* Rendering Products */}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {bestSeller.map((item, index) => (
+          <ProductItem
+            key={index}
+            id={item._id}
+            name={item.name}
+            image={item.image}
+            price={item.price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+export default BestSeller;
