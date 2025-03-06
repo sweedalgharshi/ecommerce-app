59bb5ba04f3e5b5835e369fd67aa0cfbf6b60a7f
sweedalgharshi <sweedalgracius@gmail.com>
2025-03-06 11:45:56 +0530

    Completed search functionality logic when user type something in search bar product will be displayed a/c to that keyword

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import Title from "../components/Title";
 import ProductItem from "../components/ProductItem";
 
 function Collection() {
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter, setShowFilter] = useState(false);
   const [filterProducts, setFilterProducts] = useState([]);
   const [category, setCategory] = useState([]);
@@ -31,6 +31,12 @@ function Collection() {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter((item) =>
+        item.name.toLowerCase().includes(search.toLowerCase())
+      );
+    }
 
     if (category.length > 0) {
       productsCopy = productsCopy.filter((item) => category.includes(item.category));
@@ -69,7 +75,7 @@ function Collection() {
     function () {
       applyFilter();
     },
-    [category, subCategory]
+    [category, subCategory, search, showSearch]
   );
 
   useEffect(
