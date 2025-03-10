2c4a6edc878c8cdfcf6883b8e3cb8d0e79dea771
sweedalgharshi <sweedalgracius@gmail.com>
2025-03-10 12:47:04 +0530

    Configures mongodb with mongoose and added a database server in mongodb website

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 
+import connectDB from "./config/mongodb.js";
+
 // APP CONFIG
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
 
 // middlewares
 app.use(express.json());
