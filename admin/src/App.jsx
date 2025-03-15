499e13bdf4f5fb1362ed6a1d7ab63b55a7f3bc7e
sweedalgharshi <sweedalgracius@gmail.com>
2025-03-15 12:17:24 +0530

    Added styling to navbar and sidebar component and modified app.jsx as necessary

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,17 @@
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+
 function App() {
-  return <>App</>;
+  return (
+    <div className="bg-gray-50 min-h-screen">
+      <>
+        <Navbar />
+        <hr />
+      </>
+      <div className="flex w-full">
+        <Sidebar />
+      </div>
+    </div>
+  );
 }
 export default App;
---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { assets } from "../assets/assets";
+
+function Navbar() {
+  return (
+    <div className="flex items-center py-2 px-[4%] justify-between">
+      <img src={assets.logo} className="w-[max(10%,80px)]" />
+      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm">
+        Logout
+      </button>
+    </div>
+  );
+}
+export default Navbar;
