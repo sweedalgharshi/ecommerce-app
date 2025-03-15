2a96a4e47bb6f3c0a518192153ce75ecc1034e8c
sweedalgharshi <sweedalgracius@gmail.com>
2025-03-15 12:32:22 +0530

    Added styling to login component added state in app.jsx to check if admin is logged in or no

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,16 +1,37 @@
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import Add from "./pages/Add";
+import List from "./pages/List";
+import Orders from "./pages/Orders";
+import Login from "./components/Login";
 
 function App() {
+  const [token, setToken] = useState("");
+
   return (
     <div className="bg-gray-50 min-h-screen">
-      <>
-        <Navbar />
-        <hr />
-      </>
-      <div className="flex w-full">
-        <Sidebar />
-      </div>
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          <Navbar />
+          <hr />
+
+          <div className="flex w-full">
+            <Sidebar />
+            <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={<Add />} />
+                <Route path="/list" element={<List />} />
+                <Route path="/orders" element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
